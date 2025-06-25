aa9fea612141da990cdf9a621698ce7dba8eae09
Gabs-Attuy <gabrielattuy1010@gmail.com>
2025-06-25 14:54:51 -0300

    Feat: Geração de código de barras e exibição na tela de cadastro de produto.

---
+++ src/main/java/util/GeradorCodBarras.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+/**
+ *
+ * @author gabs
+ */
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.EAN13Writer;
+import java.awt.image.BufferedImage;
+
+public class GeradorCodBarras {
+    
+    public static BufferedImage gerarCodigoBarrasEAN13(String codigo) throws WriterException {
+        if (codigo == null || codigo.length() != 12 || !codigo.matches("\\d+")) {
+            throw new IllegalArgumentException("Código EAN-13 precisa ter exatamente 12 dígitos numéricos.");
+        }
+
+        EAN13Writer writer = new EAN13Writer();
+        BitMatrix bitMatrix = writer.encode(codigo, BarcodeFormat.EAN_13, 240, 80);
+        return MatrixToImageWriter.toBufferedImage(bitMatrix);
+    }
+    
+}
